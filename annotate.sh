73a3318f589c6954d57afc4ae2fb59f2a74057c0
Jenny Wang <wang.jennifer.1996@gmail.com>
2018-11-02 15:04:02 -0400

    Squashed commit of the following:
    commit bf8c306f627c756cd51e2c1c456db9a6745122c9
    Author: Jenny Wang <41165820+jenliketen@users.noreply.github.com>
    Date:   Fri Nov 2 14:57:51 2018 -0400
    
        Delete README.md
    
    commit ec92ab6ca6e3a7f8ba57add8f30dffa38827741a
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 15:38:46 2018 -0400
    
        Add files via upload
    
    commit dce0ec118d11c5a05d99e3f23b2a4df536d75d15
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:51:44 2018 -0400
    
        Summary statistics
    
    commit f756bf4a9379107e87ee4f8495fcfe508cb49183
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:28:40 2018 -0400
    
        Add files via upload
    
    commit c7902288b1a15ffce370b9049e8ea2db8dedbddc
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:28:13 2018 -0400
    
        Delete COMPARE BETWEEN PIPELINES.xlsx
    
    commit a39f6c5f2dc1a5ca904fd83eab87fc54b785570b
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:27:53 2018 -0400
    
        Add files via upload
    
    commit 52020797929254b3e9f5dc5fff86a4ecd13b3468
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:21:11 2018 -0400
    
        Coverage summary
    
    commit 20e53b9d275ccfffb3afb0498ceb1b18fc414800
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 14:17:02 2018 -0400
    
        Delete coverage_summary.xlsx
    
    commit faad49f669e08364d6b122c9026bbea2933f446c
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 11:19:41 2018 -0400
    
        Create intersect_variants.sh
    
        Compare and contrast variants according to my pipeline
    
    commit d385ba26761d6e5d0ae6297a4475910cbd37d654
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Thu Oct 25 10:41:58 2018 -0400
    
        Create intersect_variants.R
    
        Compare and contrast variants according to the authors's format
    
    commit 5e86db9997cd65b53cf255a96e9e13f957716ca4
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Wed Oct 24 10:52:15 2018 -0400
    
        Delete SNVs.RData
    
    commit 4bb166e5287db142a1685f016874d92472482488
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Wed Oct 24 09:48:03 2018 -0400
    
        Authors' SNV and indel results
    
    commit 79ca1618272f7e023e4a7091bd81faa9dc9fc586
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Fri Oct 19 13:33:53 2018 -0400
    
        Change directory path of reference genome
    
    commit 7312902c4ea35007f8737f5bd9f7b99bd58343ab
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Fri Oct 19 13:29:31 2018 -0400
    
        Change directory paths for ref genome & intervals
    
    commit 1faa39604b93211f1e45571409dc76973de9a7ee
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Fri Oct 19 13:25:28 2018 -0400
    
        Change directory path of intervals file
    
    commit 4be30e97853421302f37d0ed0028c87f2b32946c
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Wed Oct 17 14:45:34 2018 -0400
    
        Add files via upload
    
    commit 06bad89e02f870ed73ae0981160d38fa8f19dbc8
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Wed Oct 17 14:44:51 2018 -0400
    
        Delete BREAST_CANCER_PDTX-test.zip
    
    commit 7f1e6301580d643e0b2f16a273776c97cd5fd75c
    Author: jenliketen <41165820+jenliketen@users.noreply.github.com>
    Date:   Wed Oct 17 14:43:55 2018 -0400
    
        Add files via upload

---
+++ annotate.sh
@@ -0,0 +1,68 @@
+# !/bin/bash
+# 
+# $ -cwd
+# $ -S /bin/bash
+# 
+
+# Set path for Oncotator
+export ONCOTATOR_DIR="/mnt/work1/software/oncotator/1.5.3.0"
+
+# Set paths for input and output directories
+export BIALLELIC_SNP_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/6a_biallelic_SNP"
+export BIALLELIC_INDEL_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/6b_biallelic_indel"
+export ANNOTATED_SNP_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/7a_annotated_SNP"
+export ANNOTATED_INDEL_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/7b_annotated_indel"
+export DISCARD_SNP_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/8a_discard_SNP"
+export DISCARD_INDEL_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/8b_discard_indel"
+export FINAL_SNP_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/9a_final_SNP"
+export FINAL_INDEL_DIR="/mnt/work1/users/bhklab/Users/jenny/somatic_mutations/9b_final_indel"
+
+
+# Annotate
+module load oncotator/1.5.3.0
+
+
+##### SNV #####
+SNP=$(ls $BIALLELIC_SNP_DIR | grep "vcf" | cut -f1 -d".")
+for i in $SNP
+do
+  oncotator -i VCF --db-dir $ONCOTATOR_DIR/db \
+  $BIALLELIC_SNP_DIR/$i.vcf \
+  --output_format=SIMPLE_TSV \
+  $ANNOTATED_SNP_DIR/$i.tsv hg19
+
+  echo "Annotating" $i "done"
+
+# Discard non-functional variants
+  egrep 'synonymous|non-coding|intron|intergenic|non-protein' $ANNOTATED_SNP_DIR/$i.tsv > $DISCARD_SNP_DIR/$i.tsv
+
+  echo "Discarding non-functional SNPs for" $i "done"
+
+# Keep functional variants
+  egrep -v 'synonymous|non-coding|intron|intergenic|non-protein' $ANNOTATED_SNP_DIR/$i.tsv > $FINAL_SNP_DIR/$i.tsv
+
+  echo "Extracting functional SNPs for" $i "done"
+done
+
+
+##### INDEL #####
+INDEL=$(ls $BIALLELIC_INDEL_DIR | grep "vcf" | cut -f1 -d".")
+for i in $INDEL
+do
+  oncotator -i VCF --db-dir $ONCOTATOR_DIR/db \
+  $BIALLELIC_INDEL_DIR/$i.vcf \
+  --output_format=SIMPLE_TSV \
+  $ANNOTATED_INDEL_DIR/$i.tsv hg19
+
+  echo "Annotating" $INDEL_ID "indels done"
+
+# Discard non-functional variants
+  egrep 'intron|intergenic|non-protein' $ANNOTATED_INDEL_DIR/$i.tsv > $DISCARD_INDEL_DIR/$i.tsv
+
+  echo "Discarding non-functional indels for" $i "done"
+
+# Keep functional variants
+  egrep -v 'intron|intergenic|non-protein' $ANNOTATED_INDEL_DIR/$i.tsv > $FINAL_INDEL_DIR/$i.tsv
+
+  echo "Extracting functional indels for" $i "done"
+done
