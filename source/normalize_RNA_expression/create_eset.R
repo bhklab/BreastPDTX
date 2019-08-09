43f37c381691155fe15418e441b07fea94bec104
Jenny Wang <41165820+jenliketen@users.noreply.github.com>
2019-08-09 10:26:08 -0400

    Source code for creating final ExpressionSet

---
+++ source/normalize_RNA_expression/create_eset.R
@@ -0,0 +1,17 @@
+library(Biobase)
+
+expression <- readRDS("~/Desktop/normalize_RNA_expression/after_normalization.Rda")
+m <- exprs(expression)
+pdata <- pData(expression); rownames(pdata) <- pdata$ID
+fdata <- fData(expression); rownames(fdata) <- make.names(fdata$symbol, unique=TRUE)
+
+colnames(m) <- pdata$ID; rownames(m) <- rownames(fdata)
+m <- m[order(rownames(m)), order(colnames(m))]
+
+pdata <- pdata[order(pdata$ID), ]
+pdata <- AnnotatedDataFrame(pdata)
+fdata <- fdata[order(rownames(fdata)), ]
+fdata <- AnnotatedDataFrame(fdata)
+eset <- new("ExpressionSet", exprs=m, phenoData=pdata, featureData=fdata)
+
+saveRDS(eset, file="~/Desktop/normalize_RNA_expression/final_eset.Rda")
